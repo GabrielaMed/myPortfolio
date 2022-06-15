2c07a380eb4c73138160ff6e6d63fc2fc0e3443d
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2022-06-15 12:43:43 -0400

    css change

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ gsap.from('.myName', {
   y: 30,
   ease: 'expo.out'
 })
+
 gsap.from('.btns', {
   opacity: 0,
   duration: 3,
@@ -17,17 +18,23 @@ gsap.from('.btns', {
 })
 
 /*Text*/
-gsap.from('.hi', {
+gsap.from('.subtitle', {
   opacity: 0,
   duration: 3,
   delay: 1.3,
   y: 35,
   ease: 'expo.out'
 })
-gsap.from('.name-title', {
+
+gsap.from('.title', {
   opacity: 0,
   duration: 3,
   delay: 1.1,
   y: 35,
   ease: 'expo.out'
+})
+
+const sr = ScrollReveal({
+  duration: 2500,
+  reset: true
 })
