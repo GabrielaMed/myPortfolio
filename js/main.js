66e5537de4c09e69f36d9dd408b1d5001338f6d0
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2022-06-17 11:03:34 -0400

    section about done

--- js/main.js
+++ js/main.js
@@ -1,29 +1,28 @@
 /* home animation */
 
 /* Mountains animation */
-let imgOne = document.getElementById('imgOne');
-let imgTwo = document.getElementById('imgTwo');
-let imgThree = document.getElementById('imgThree');
-let imgFour = document.getElementById('imgFour');
-let title = document.getElementById('title');
-let subtitle = document.getElementById('subtitle');
+let imgOne = document.getElementById('imgOne')
+let imgTwo = document.getElementById('imgTwo')
+let imgThree = document.getElementById('imgThree')
+let imgFour = document.getElementById('imgFour')
+let title = document.getElementById('title')
+let subtitle = document.getElementById('subtitle')
 
-window.addEventListener('scroll', function(){
-  var value = window.scrollY;
+window.addEventListener('scroll', function () {
+  var value = window.scrollY
 
-  imgOne.style.top = value * 0.5 + 'px';
-  imgTwo.style.top = value * 0.5 + 'px';
-  imgThree.style.top = value * 0.15 + 'px';
-  imgFour.style.top = value * 0.15 + 'px';
-  title.style.top = value * 1 + 'px';
-  subtitle.style.top = value * 1 + 'px';
+  imgOne.style.top = value * 0.5 + 'px'
+  imgTwo.style.top = value * 0.5 + 'px'
+  imgThree.style.top = value * 0.15 + 'px'
+  imgFour.style.top = value * 0.15 + 'px'
+  title.style.top = value * 1 + 'px'
+  subtitle.style.top = value * 1 + 'px'
 })
 
-
 /* Animation on reload */
-ScrollReveal().reveal('.myName', { interval: 16, reset: true, delay: 300 });
-ScrollReveal().reveal('.btn', { interval: 16, reset: true, delay: 300 });
-ScrollReveal().reveal('.title', { interval: 16, reset: true, delay: 400 });
-ScrollReveal().reveal('.subtitle', { interval: 16, reset: true, delay: 300 });
+ScrollReveal().reveal('.myName', { interval: 16, reset: true, delay: 300 })
+ScrollReveal().reveal('.btn', { interval: 16, reset: true, delay: 300 })
+ScrollReveal().reveal('.title', { interval: 16, reset: true, delay: 400 })
+ScrollReveal().reveal('.subtitle', { interval: 16, reset: true, delay: 300 })
 
 /* other animation */
